8c80377dfc732cdff64a13c40e0b74e70393882d
namreeb <github@namreeb.org>
2016-09-20 15:35:25 -1000

    - decrease tile precision to allow generation to complete

--- RecastDetourBuild/Include/Common.hpp
+++ RecastDetourBuild/Include/Common.hpp
@@ -15,7 +15,7 @@ class MeshSettings
 {
     public:
         static constexpr int TilesPerChunk = 3;                 // number of rows and columns of tiles per ADT MCNK chunk
-        static constexpr int TileVoxelSize = 56;                // number of voxel rows and columns per tile
+        static constexpr int TileVoxelSize = 38;                // number of voxel rows and columns per tile
 
         static constexpr float CellHeight = 0.5f;
         static constexpr float WalkableHeight = 1.6f;           // agent height in world units (yards)
