b4d7fc2c5b440f25a0cc0d77196e95b443484b95
Gtker <git@gtker.com>
2022-10-11 17:37:02 +0200

    Make C API pointers const
    This ensures that the underlying object can still be mutated but the
    pointer location can not.
    
    This just ensures that calling functions don't have to be overly
    defensive about what is happening with pointers passed to the API.
    For example the Map pointer can't be changed to a nullptr, or to another
    Map object.

--- pathfind/pathfind_c_bindings.hpp
+++ pathfind/pathfind_c_bindings.hpp
@@ -17,37 +17,37 @@ pathfind::Map* pathfind_new_map(const char* const data_path,
                                 const char* const map_name,
                                 PathfindResultTypePtr result);
 
-void pathfind_free_map(pathfind::Map* map);
+void pathfind_free_map(pathfind::Map* const map);
 
-PathfindResultType pathfind_load_all_adts(pathfind::Map* map,
-                                          int32_t* amount_of_adts_loaded);
+PathfindResultType pathfind_load_all_adts(pathfind::Map* const map,
+                                          int32_t* const amount_of_adts_loaded);
 
-PathfindResultType pathfind_load_adt(pathfind::Map* map, int adt_x, int adt_y,
-                                     float* out_adt_x, float* out_adt_y);
+PathfindResultType pathfind_load_adt(pathfind::Map* const map, int adt_x, int adt_y,
+                                     float* const out_adt_x, float* const out_adt_y);
 
-PathfindResultType pathfind_load_adt_at(pathfind::Map* map, float x, float y,
-                                        float* out_adt_x, float* out_adt_y);
+PathfindResultType pathfind_load_adt_at(pathfind::Map* const map, float x, float y,
+                                        float* const out_adt_x, float* const out_adt_y);
 
-PathfindResultType pathfind_get_zone_and_area(pathfind::Map* map, float x,
+PathfindResultType pathfind_get_zone_and_area(pathfind::Map* const map, float x,
                                               float y, float z,
-                                              unsigned int* out_zone,
-                                              unsigned int* out_area);
+                                              unsigned int* const out_zone,
+                                              unsigned int* const out_area);
 
-PathfindResultType pathfind_find_path(pathfind::Map* map, float start_x,
+PathfindResultType pathfind_find_path(pathfind::Map* const map, float start_x,
                                       float start_y, float start_z,
                                       float stop_x, float stop_y, float stop_z,
-                                      Vertex* buffer,
+                                      Vertex* const buffer,
                                       unsigned int buffer_length,
-                                      unsigned int* amount_of_vertices);
+                                      unsigned int* const amount_of_vertices);
 
-PathfindResultType pathfind_find_heights(pathfind::Map* map, float x, float y,
-                                         float* buffer,
+PathfindResultType pathfind_find_heights(pathfind::Map* const map, float x, float y,
+                                         float* const buffer,
                                          unsigned int buffer_length,
-                                         unsigned int* amount_of_heights);
+                                         unsigned int* const amount_of_heights);
 
-PathfindResultType pathfind_find_height(pathfind::Map* map, float start_x,
+PathfindResultType pathfind_find_height(pathfind::Map* const map, float start_x,
                                         float start_y, float start_z,
                                         float stop_x, float stop_y,
-                                        float* stop_z);
+                                        float* const stop_z);
 } // extern "C"
 
